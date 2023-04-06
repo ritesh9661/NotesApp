53104ccc86611d77e20f57ff5f5029d109c225d8
Ritesh Kumar Yadav <ritesh@itobuz.com>
2023-04-06 19:30:55 +0530

    3.0.2

--- frontEnd/js/main.js
+++ frontEnd/js/main.js
@@ -13,11 +13,11 @@ const getAllNotes = () => {
         // console.log(apiId);
         document.getElementById(
           "display"
-        ).innerHTML += `<div class="cards col-3 text-center  ">
-          <div class="display_title">${json.data[i].title}</div>
-          <div class="text-start d-flex flex-column ">${json.data[i].content}</div>
-          <button class="btn btn-light my-3 p-2" id="deleteNote" onclick="del('${apiId}')">Delete</button>
-            </div>`;
+        ).innerHTML += `<div class="cards col-5  text-center  ">
+           <div class="display_title ">${json.data[i].title}</div>
+          <div class="text-start d-flex  justify-content-between align-items-center  text-wrap"><div>${json.data[i].content}</div>
+          <button class="btn btn-light my-3 col-2 p-2" id="deleteNote " onclick="del('${apiId}')"><img src="../trash.svg" alt=""></button>
+          </div></div>`;
         document.getElementById("title").value = "";
         document.getElementById("dec").value = "";
       }
@@ -30,11 +30,10 @@ function del(i) {
   fetch(`http://localhost:8002/notes/delete/${i}`, {
     method: "DELETE",
   });
-
   getAllNotes();
 }
 
-function add(event) {
+function add() {
   let title = document.getElementById("title").value;
   let content = document.getElementById("dec").value;
   if (title === "" || content === "") {
@@ -54,14 +53,14 @@ function add(event) {
       .then((json) => {
         document.getElementById(
           "display"
-        ).innerHTML += `<div class=" cards col-3 text-center  ">
+        ).innerHTML += `<div class=" cards col-4  text-center  ">
           <div class="display_title" id="final_title">${
             document.getElementById("title").value
           }</div>
           <div class="text-start d-flex flex-column " id="final_desc">${
             document.getElementById("dec").value
           }</div>
-          <button class="btn btn-light my-3 p-2" >Delete</button>
+          <button class="btn btn-light my-3 p-2" ><img src="../trash.svg" alt=""></button>
       </div>`;
         document.getElementById("title").value = "";
         document.getElementById("dec").value = "";
